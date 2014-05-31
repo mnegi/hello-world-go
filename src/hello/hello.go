c6e20704e8ad24af88a7e55e61f282c34329e3e9
Manohar Negi <manohar.negi@komli.com>
2014-05-31 19:04:21 +1000

    added comment

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,12 @@
+// Package main : Executable commands must always use package main.
 package main
 
+// import standard fmt package  for formatted I/O functions
 import "fmt"
 
+// main function : the starting point of an executable go program
 func main(){
+
+	// print hello world using fmt packages' Printf function
 	fmt.Printf("Hello world!\n")
 }
